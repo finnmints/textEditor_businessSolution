13a305e2e6c6b7598257149f011aa494fd166574
finn <finnw16@nycstudents.net>
2025-12-15 14:28:37 -0500

    Update textEditorPT.py

--- textEditorPT.py
+++ textEditorPT.py
@@ -1,8 +1,16 @@
 from tkinter import *
 from tkmacosx import Button
 
+# Create the main window
+root = Tk()
+root.title("NoteBook")
+
+#Set size of window
+root.geometry("300x150")
+
 # Add in the page selector w scrollbar
-pageList = Listbox()
+pageLabel = Label(root, text="Pages")
+pageList = Listbox(root, height=50, width=200)
 pageList.insert(1, "page1")
 pageList.insert(2, "page2")
 pageList.insert(3, "page3")
@@ -12,3 +20,6 @@ scroll = Scrollbar(pageList)
 
 pageList.pack()
 scroll.pack()
+
+# Start the GUI event loop
+root.mainloop()
