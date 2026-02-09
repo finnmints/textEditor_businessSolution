e6e6395dc1433fe5ecd8269924a01e585ae5e80f
finn <finnw16@nycstudents.net>
2026-02-09 14:28:29 -0500

    f

--- Lesson10_BM.py
+++ Lesson10_BM.py
@@ -7,8 +7,8 @@ root = Tk()
 
 def Write_clicked():
 	with open("demofile.txt", "w") as f:
-		f.write("")
-	text = Textbox.get("1.0", END)
+		text = Textbox.get("1.0", END)
+		f.write(text)
 	print("You entered:")
 	print(text)
 
@@ -18,18 +18,46 @@ def Read_clicked():
 		print(content)
 		Textbox.insert("1.0", content)
 
+def Red_clicked():
+	root.configure(background = 'indianRed')
+
+def Green_clicked():
+	root.configure(background = 'DarkSeaGreen1')
+
+def Blue_clicked():
+	root.configure(background = 'Aliceblue')
+
+def Yellow_clicked():
+	root.configure(background = 'lemonchiffon')
+
 WriteButton = Button(root, text="Write To File", command = Write_clicked)
 ReadButton = Button(root, text="Read From File", command = Read_clicked)
 
 Textbox = Text(root)
 
 with open('demofile.txt', 'w') as file:
-    file.write('Textbox')
+    file.write('demofile.txt')
 with open('demofile.txt', 'r') as file:
     content = file.read()
+
+menubutton = Menubutton(root, text = "Background Color")   
+  
+menubutton.menu = Menu(menubutton)  
+menubutton["menu"]= menubutton.menu  
+
+var1 = IntVar()
+var2 = IntVar()
+var3 = IntVar()
+var4 = IntVar()
+
+menubutton.menu.add_checkbutton(label = "Green", variable = var1, command = Green_clicked)  
+menubutton.menu.add_checkbutton(label = "Yellow", variable = var2, command = Yellow_clicked)
+menubutton.menu.add_checkbutton(label = "Blue", variable = var3, command = Blue_clicked)
+menubutton.menu.add_checkbutton(label = "Red", variable = var4, command = Red_clicked)
 
 WriteButton.pack()
 ReadButton.pack()
 Textbox.pack()
+menubutton.pack()
 
 root.mainloop()
